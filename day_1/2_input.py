87f0b755e8a6a68f3dc716f9965bd4d748d56689
student-eng <student-eng@EDU-WSB.local>
2026-05-11 10:23:47 +0200

    '1'

---
+++ day_1/2_input.py
@@ -0,0 +1,9 @@
+name = input('What is your name?   ')
+print(name)
+
+age = int(input('How old are you?   '))
+# print('you are')
+# print(age)
+# print('years old')
+print(f'You are {age} years old')
+print(f'You were born in {2026-age} year')
