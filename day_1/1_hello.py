7a03b2c660dd9b29300f39c94ea353d231266790
student-eng <student-eng@EDU-WSB.local>
2026-05-11 10:08:06 +0200

    '1'

---
+++ day_1/1_hello.py
@@ -0,0 +1,15 @@
+print('Hello')
+a = 5
+b = 7.5
+c = 'doggy'
+d = 'Python'
+print(a) # print variable a -> 5
+print('a') # print letter 'a'
+print(a + b)  # add 2 numbers
+# print(a + c) # add number and text - wrong
+print(a * c) # mutiply numer and text
+print(c + d) # combine text
+
+print(type(a))
+print(type(b))
+print(type(c))
