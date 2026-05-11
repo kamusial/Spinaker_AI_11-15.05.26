ef650d057dacd09f4b5511cd49393d8ea33d2235
student-eng <student-eng@EDU-WSB.local>
2026-05-11 12:04:58 +0200

    '1'

--- day_1/5_lists_strings.py
+++ day_1/5_lists_strings.py
@@ -9,3 +9,16 @@ print(my_list)   # display whole list
 print(my_list[3])   # display 4th element
 # print(my_list[5])  # 6th element
 
+word = input('Give a word:  ')
+
+# for i in range(len(word)):
+#     if word[i] == 'a':
+#         print(f'Yes, I found letter a in possition {i+1}')
+
+counter_a = 0
+for letter in word:
+    # print(letter)
+    if letter == 'a':
+        print('Yes, I found letter a')
+        counter_a = counter_a + 1
+print(f'I found {counter_a} letters a')
---
+++ day_1/6_dictionaries.py
@@ -0,0 +1,13 @@
+# shopping_list = ['bananas', 1,  'oranges', 3, 'milk', 5]
+
+my_dict = {'banana': 5, 'orange': 8, 'milk': 8}
+print(my_dict['orange'])
+
+users_dict = {'Kalim01': 'xxx1', 'John': 'John123'}
+print(users_dict)
+users_dict['John'] = 'new_password'
+print(users_dict)
+users_dict['Marley'] = 'MarleyPassword'
+print(users_dict)
+
+
