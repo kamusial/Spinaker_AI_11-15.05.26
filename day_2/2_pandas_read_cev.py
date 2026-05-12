36469838b95c386dd16cde420c15ed1c97cef00e
student-eng <student-eng@EDU-WSB.local>
2026-05-12 12:51:54 +0200

    '1'

--- day_2/2_pandas_read_cev.py
+++ day_2/2_pandas_read_cev.py
@@ -15,3 +15,15 @@ plt.hist(df.query("Gender=='Female'")['Weight'], bins=30)
 # plt.show()
 plt.hist(df.query("Gender=='Male'")['Weight'], bins=30)
 plt.show()
+
+df = pd.get_dummies(df)
+del df['Gender_Male']
+df = df.rename(columns={'Gender_Female': 'Gender'})
+# 1 / True = Female    0 / False = Male
+print(df)
+
+from sklearn.linear_model import LinearRegression
+model = LinearRegression()
+model.fit(df[['Height', 'Gender']], df['Weight'])
+print(model.coef_)
+print(model.intercept_)
