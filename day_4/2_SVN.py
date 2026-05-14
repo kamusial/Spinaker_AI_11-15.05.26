5491ce928fbe8fed68f6411949ece11f3a956b2a
student-eng <student-eng@EDU-WSB.local>
2026-05-14 11:14:40 +0200

    '1'

---
+++ day_4/2_SVN.py
@@ -0,0 +1,27 @@
+from sklearn.svm import SVC
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import confusion_matrix
+
+df = pd.read_csv('iris.csv')
+print(df['class'].value_counts())
+print(df)
+
+species = {
+    'Iris-setosa': 0, 'Iris-versicolor': 1, 'Iris-virginica':2
+}
+df['class_value'] = df['class'].map(species)
+print(df['class_value'].value_counts())
+
+X = df.iloc[:, :2]
+y = df.class_value
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model = SVC()
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
---
+++ day_4/3_heart_4_algorithms.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+
+df = pd.read_csv("heart.csv", comment='#')
+X = df.iloc[: , :-1]
+y = df.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+print('\nLogistic Regression')
+model = LogisticRegression()
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
+print('\nKNN')
+model = KNeighborsClassifier(n_neighbors=5, weights='distance')
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
+print('\nDecission Tree')
+model = DecisionTreeClassifier(max_depth=4, min_samples_split=5, criterion='gini')
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
+print('\nSVN')
+model = SVC(kernel='rbf')
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
