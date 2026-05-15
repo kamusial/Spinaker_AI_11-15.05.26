21683e92b4946664dc74fe8060678b3ad26d284f
student-eng <student-eng@EDU-WSB.local>
2026-05-15 09:53:52 +0200

    '1'

--- day_5/2_sinus_exp.py
+++ day_5/2_sinus_exp.py
@@ -1,14 +1,15 @@
+import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 import random
 
-x = np.arange(-4 * np.pi, 4 * np.pi, 0.2)
-y = np.array([np.sin(number) * random.uniform(0.7, 1.3) + random.uniform(-0.1, 0.1) for number in x])
+x = np.arange(-8 * np.pi, 8 * np.pi, 0.4)
+y = np.array([np.sin(number) * np.exp(-0.05 * number) * random.uniform(0.5, 1.5) for number in x])
 
-# plt.figure(figsize=(10, 5))
-# plt.scatter(x, y, label='sin(x)', color='blue', linewidth=2)
+# plt.figure(figsize=(10, 5))  # Ustawienie rozmiaru wykresu
+# plt.scatter(x, y, label='sin(x)', color='blue')  # Narysowanie funkcji sinus
 # plt.title('Chart sinus')
 # plt.xlabel('x')
 # plt.ylabel('sin(x)')
@@ -22,29 +23,28 @@ y = np.array([np.sin(number) * random.uniform(0.7, 1.3) + random.uniform(-0.1, 0
 # )
 # plt.show()
 
-# build neural network like yesterday - 10 minutes
 model = Sequential()
 model.add(Dense(1, input_dim=1, activation='linear'))
-model.add(Dense(20, activation='linear'))
-model.add(Dense(20, activation='relu'))
-model.add(Dense(20, activation='linear'))
-model.add(Dense(20, activation='sigmoid'))
-model.add(Dense(20, activation='linear'))
-model.add(Dense(1, activation='linear'))
+model.add(Dense(5, activation='relu'))
+model.add(Dense(5, activation='relu'))
+model.add(Dense(5, activation='linear'))
+model.add(Dense(1))
+model.compile(optimizer='adam', loss='mse')
 
-model.compile(optimizer='rmsprop', loss='mse')
-result = model.fit(x, y, epochs=20000)
-
+x_reshape = x.reshape(-1, 1)
+y_reshape = y.reshape(-1, 1)
+model.fit(x_reshape, y_reshape, epochs=100, verbose=2)
 y_pred = model.predict(x)
 plt.scatter(x, y)
 plt.plot(x, y_pred, c='r')
+
 plt.title('Chart sinus')
 plt.xlabel('x')
 plt.ylabel('sin(x)')
 plt.grid(True, linestyle='--', alpha=0.7)
 plt.legend()
-plt.axhline(0, color='black', linewidth=0.5)  # Oś X
-plt.axvline(0, color='black', linewidth=0.5)  # Oś Y
+plt.axhline(0, color='black', linewidth=0.5)
+plt.axvline(0, color='black', linewidth=0.5)
 plt.xticks(
     ticks=[-2*np.pi, -3*np.pi/2, -np.pi, -np.pi/2, 0, np.pi/2, np.pi, 3*np.pi/2, 2*np.pi],
     labels=['-2π', '-3π/2', '-π', '-π/2', '0', 'π/2', 'π', '3π/2', '2π']
